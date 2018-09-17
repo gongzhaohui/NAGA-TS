1c782d0061c78b1389bfa0256c1190c625b25f64
Zhaohui Gong <gongzhaohui@gmail.com>
2018-09-17 16:45:55 +0800

    Auth yet

--- src/server/modules/database/base.service.ts
+++ src/server/modules/database/base.service.ts
@@ -1,10 +1,8 @@
-import { Injectable } from '@nestjs/common';
-@Injectable()
-export abstract class BaseService {
+abstract class BaseService {
   abstract collection: any;
 
-  async getAll(): Promise<any> {
-    const rst: any = await this.collection.all();
+  async getAll(opts: any): Promise<any[]> {
+    const rst: any = await this.collection.all(opts);
     return rst._result;
   }
   async getByKey(_key: string): Promise<any> {
@@ -17,8 +15,8 @@ export abstract class BaseService {
   async getByBindVars(bindVars: object): Promise<any> {
     return await this.collection.firstExample(bindVars);
   }
-  async updateBykey(_key: string, body: any): Promise<any> {
-    return await this.collection.update(_key, body, { returnNew: true });
+  async updateBykey(_key: string, body: any, opts: any): Promise<any> {
+    return await this.collection.update(_key, body, opts);
   }
   async insertOne(body: any): Promise<any> {
     return await this.collection.save(body, { returnNew: true });
@@ -29,5 +27,8 @@ export abstract class BaseService {
   }
   async deleteByKeys(_keys: string[]): Promise<any> {
     return await this.collection.removeByKeys([..._keys], {});
+  }
+  async count(): Promise<any> {
+    return await this.collection.count();
   }
 }
