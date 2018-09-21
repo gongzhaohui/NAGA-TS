9210c29c0e0e9b8a8d326e29f230cef149ca1815
Zhaohui Gong <gongzhaohui@gmail.com>
2018-09-21 16:34:28 +0800

    jwt strategy passReqToCallback

--- src/server/modules/auth/passport/jwt.strategy.ts
+++ src/server/modules/auth/passport/jwt.strategy.ts
@@ -13,6 +13,7 @@ export class JwtStrategy extends PassportStrategy(Strategy) {
     super({
       jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
       secretOrKey: SERVER_CONFIG.jwtSecret,
+      passReqToCallback: true,
     });
   }
 
